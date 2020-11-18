65f47bdac2dac41d8d7c2929f107b5ec58743168
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2020-11-18 15:41:28 -0800

    start

--- game.py
+++ game.py
@@ -18,6 +18,10 @@ class Weapon:
 	:param power: Le niveau d'attaque
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
+	def __init__(self, nom, power, min_level):
+		self.nom = nom
+		self.power = power
+		self.min_level = min_level
 
 	UNARMED_POWER = 20
 
@@ -32,7 +36,17 @@ class Character:
 	:param defense: Le niveau de défense du personnage
 	:param level: Le niveau d'expérience du personnage
 	"""
+	def __init__(self, name, max_hp, attack, defense, level, weapon):
+		self.name = name
+		self.max_hp = max_hp
+		self.attack = attack
+		self.defense = defense
+		self.level = level
+		self.weapon = weapon
 
+	def compute_damage(self, defender: "Character"):
+		modifier = (1 if random.random() > 0.0625 else 2) * random.randint(85, 100)/100
+		return (2 + ((2 + 2 * self.level/5) * self.weapon.power * self.attack/defender.defense)/50) * modifier
 
 
 def deal_damage(attacker, defender):
@@ -41,6 +55,7 @@ def deal_damage(attacker, defender):
 	if crit:
 		print("  Critical hit!")
 	print(f"  {defender.name} took {damage} dmg")
+
 
 def run_battle(c1, c2):
 	# TODO: Initialiser attaquant/défendeur, tour, etc.
